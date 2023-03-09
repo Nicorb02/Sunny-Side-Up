dd91219fc671eea104da217e6b18c739a30af1bc
nader-fares <naderfares@outlook.com>
2023-03-08 23:06:55 -0500

    run react

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const path = require("path")
+const PORT = process.env.PORT || 8000;
+
 const app = express();
+
+
 
 mongoose.connect("mongodb+srv://user123:Group22Rules@COP4331.bvp84gt.mongodb.net/COP4331?retryWrites=true&w=majority")
 
+app.set('port', (process.env.PORT || 8000))
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => 
@@ -20,4 +28,7 @@ app.use((req, res, next) =>
   );
   next();
 });
+
+
+
 app.listen(8000); // start Node + Express server on port 8000
