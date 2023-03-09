e2540c2070da58bfae4f81d6fad13de1ea96c322
nader-fares <naderfares@outlook.com>
2023-03-08 22:16:57 -0500

    add user schema

---
+++ User.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose")
+const { boolean } = require("webidl-conversions")
+
+const eventSchema = new mongoose.Schema({
+    name: {String},
+    startTime: Date,
+    EndTime: Date,
+    note: String,
+    type: Number,
+    complete: Boolean
+})
+
+const contactSchema = new mongoose.Schema([{
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    },
+    phone: {
+        type: String,
+        required: true,
+        length: 10,
+    },
+}],)
+
+
+const plannerSchema = new mongoose.Schema({
+    notes: {
+        name: String,
+        content: String,
+    },
+
+    contacts: contactSchema,
+
+    holidays: [Date],
+
+    events: eventSchema,
+})
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    }, 
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+
+    }, 
+    password: {
+        type: String,
+        required: true,
+    },
+
+    planner: plannerSchema,
+})
+
+module.exports = mongoose.model("User", userSchema)
