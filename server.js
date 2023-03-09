ff9673c9d1c725616347c69bfbc877a169ac273d
nader-fares <naderfares@outlook.com>
2023-03-08 23:35:38 -0500

    express setup

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const path = require("path")
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-
+require("dotenv").config()
 
 
 mongoose.connect("mongodb+srv://user123:Group22Rules@COP4331.bvp84gt.mongodb.net/COP4331?retryWrites=true&w=majority")
 
 app.set('port', (process.env.PORT || 8000))
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => 
@@ -21,14 +23,17 @@ app.use((req, res, next) =>
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, DELETE, OPTIONS'
+      );
+      next();
+    });
+    
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+    
 
-
-
-app.listen(8000); // start Node + Express server on port 8000
+app.listen(PORT); // start Node + Express server on port 8000
