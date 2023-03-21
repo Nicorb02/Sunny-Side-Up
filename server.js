bc25bc6bbb093848a802680a4637b456ed6c789d
Nicolas Rodriguez <nicorb02@gmail.com>
2023-03-21 00:59:28 -0400

    boiler plate api stuff

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 8080));
 
-const url = 'mongodb+srv://RickLeinecker:COP4331Rocks@cluster0.ehunp00.mongodb.net/?retryWrites=true&w=majority';
+// const url = "mongodb+srv://user123:Group22Rules@COP4331.bvp84gt.mongodb.net/COP4331?retryWrites=true&w=majority";
 require('dotenv').config();
-//const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI;
 const MongoClient = require("mongodb").MongoClient;
 const client = new MongoClient(url);
 client.connect(console.log("mongodb connected"));
@@ -27,179 +27,51 @@ app.use((req, res, next) =>
     next();
 });
 
-var cardList =
-[
-'Roy Campanella',
-'Paul Molitor',
-'Tony Gwynn',
-'Dennis Eckersley',
-'Reggie Jackson',
-'Gaylord Perry',
-'Buck Leonard',
-'Rollie Fingers',
-'Charlie Gehringer',
-'Wade Boggs',
-'Carl Hubbell',
-'Dave Winfield',
-'Jackie Robinson',
-'Ken Griffey, Jr.',
-'Al Simmons',
-'Chuck Klein',
-'Mel Ott',
-'Mark McGwire',
-'Nolan Ryan',
-'Ralph Kiner',
-'Yogi Berra',
-'Goose Goslin',
-'Greg Maddux',
-'Frankie Frisch',
-'Ernie Banks',
-'Ozzie Smith',
-'Hank Greenberg',
-'Kirby Puckett',
-'Bob Feller',
-'Dizzy Dean',
-'Joe Jackson',
-'Sam Crawford',
-'Barry Bonds',
-'Duke Snider',
-'George Sisler',
-'Ed Walsh',
-'Tom Seaver',
-'Willie Stargell',
-'Bob Gibson',
-'Brooks Robinson',
-'Steve Carlton',
-'Joe Medwick',
-'Nap Lajoie',
-'Cal Ripken, Jr.',
-'Mike Schmidt',
-'Eddie Murray',
-'Tris Speaker',
-'Al Kaline',
-'Sandy Koufax',
-'Willie Keeler',
-'Pete Rose',
-'Robin Roberts',
-'Eddie Collins',
-'Lefty Gomez',
-'Lefty Grove',
-'Carl Yastrzemski',
-'Frank Robinson',
-'Juan Marichal',
-'Warren Spahn',
-'Pie Traynor',
-'Roberto Clemente',
-'Harmon Killebrew',
-'Satchel Paige',
-'Eddie Plank',
-'Josh Gibson',
-'Oscar Charleston',
-'Mickey Mantle',
-'Cool Papa Bell',
-'Johnny Bench',
-'Mickey Cochrane',
-'Jimmie Foxx',
-'Jim Palmer',
-'Cy Young',
-'Eddie Mathews',
-'Honus Wagner',
-'Paul Waner',
-'Grover Alexander',
-'Rod Carew',
-'Joe DiMaggio',
-'Joe Morgan',
-'Stan Musial',
-'Bill Terry',
-'Rogers Hornsby',
-'Lou Brock',
-'Ted Williams',
-'Bill Dickey',
-'Christy Mathewson',
-'Willie McCovey',
-'Lou Gehrig',
-'George Brett',
-'Hank Aaron',
-'Harry Heilmann',
-'Walter Johnson',
-'Roger Clemens',
-'Ty Cobb',
-'Whitey Ford',
-'Willie Mays',
-'Rickey Henderson',
-'Babe Ruth'
-];
-
-app.post('/api/addcard', async (req, res, next) =>
+// need to configure database stuff (apis and file as whole)
+app.post('/api/login', async (req, res) =>
 {
-    // incoming: userId, color
-    // outgoing: error
-    const { userId, card } = req.body;
-    const newCard = {Card:card,UserId:userId};
+  // incoming: email, password
+  // outgoing: id, firstName, lastName, error
+  var error = '';
+  const { email, password } = req.body;
+  const db = client.db(/Database Name/);
+  const results = await db.collection('Users').find({Email:email,Password:password}).toArray();
+  var id = -1;
+  var fn = '';
+  var ln = '';
+  if( results.length > 0 )
+    {
+    id = results[0].UserID;
+    fn = results[0].FirstName;
+    ln = results[0].LastName;
+    }
+  var ret = { id:id, firstName:fn, lastName:ln, error:''};
+  res.status(200).json(ret);
+});
+
+app.post('/api/register', async(req,res)=>{
+  
+    // incoming: firstName, lastName, email, password
+    // outgoing: error (if applicable)
+  
     var error = '';
-    
+    const {firstName, lastName, email, password} = req.body;
+    const newUser = {FirstName:firstName, LastName:lastName, Email:email, Password:password};
     try
     {
-        const db = client.db("COP4331Cards");
-        const result = db.collection('Cards').insertOne(newCard);
+      const db = client.db(/Database Name/);
+      db.collection("Users").insertOne(newUser);
     }
     catch(e)
     {
-        error = e.toString();
+      error = e.toString();
     }
-
-    cardList.push( card );
-    var ret = { error: error };
+  
+    var ret = {error: error};
     res.status(200).json(ret);
-});
+  })
 
-app.post('/api/login', async (req, res, next) =>
-{
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
-    var error = '';
-    const { login, password } = req.body;
-    const db = client.db("COP4331Cards");
-    
-    const results = await
-    db.collection('Users').find({Login:login,Password:password}).toArray();
-    
-    var id = -1;
-    var fn = '';
-    var ln = '';
-    
-    if( results.length > 0 )
-    {
-        id = results[0].UserID;
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
-    }
-    
-    var ret = { id:id, firstName:fn, lastName:ln, error:''};
-    res.status(200).json(ret);
-});
-
-
-app.post('/api/searchcards', async (req, res, next) =>
-{
-    // incoming: userId, search
-    // outgoing: results[], error
-    var error = '';
-    const { userId, search } = req.body;
-    var _search = search.trim();
-    const db = client.db("COP4331Cards");
-    const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
-    var _ret = [];
-    
-    for( var i=0; i<results.length; i++ )
-    {
-        _ret.push( results[i].Card );
-    }
-
-    var ret = {results:_ret, error:error};
-    res.status(200).json(ret);
-});
-
+// ======= HEROKU DEPLOYMENT (DO NOT MODIFY) ========
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production')
 {
