f597130bd1731d89bfe5445e31d53564ca5a30b1
Nicorb02 <98073598+Nicorb02@users.noreply.github.com>
2023-03-23 18:02:37 -0400

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -28,7 +28,7 @@ function Login() {
   {
     if (process.env.NODE_ENV === 'production')
     {
-        return 'https://' + app_name + '.herokuapp.com/' + route;
+        return 'https://' + app_name + '.herokuapp.com' + route;
     }
     else
     {
