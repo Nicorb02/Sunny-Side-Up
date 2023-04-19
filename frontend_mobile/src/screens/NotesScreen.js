175d58c1ce56fde91706ca44d5aababac2cafe7e
nader-fares <naderfares@outlook.com>
2023-04-19 04:11:07 -0400

    implement edit note

--- frontend_mobile/src/screens/NotesScreen.js
+++ frontend_mobile/src/screens/NotesScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, FlatList, TouchableOpacity , Modal, TextInput, ScrollView} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Card, Avatar } from "react-native-paper";
@@ -24,13 +24,28 @@ const NotesScreen = () => {
     const [content, setContent] = useState('')
 
     const [addNoteModal, setAddNoteModal] = useState(false)
+    const [editNoteModal, setEditNoteModal] = useState(false)
 
+    const [editItem, setEditItem] = useState({id: null})
 
     let count = notes.length
+
+
 
     const deleteNote = (id) => {
         setNotes((prevData) => prevData.filter((note) => note.id !== id));
     };
+    
+    const editNote = () => {
+        setNotes((prevData) =>
+        prevData.map((item) =>
+        item.id === editItem.id
+          ? { ...item, title: title, content: content }
+          : item
+        )
+        );
+        setEditNoteModal(false);
+    }
 
     const addNote = () => {
         notes.push(
@@ -47,33 +62,12 @@ const NotesScreen = () => {
         setAddNoteModal(true)
     }
 
-
-
-    const renderItems = ({ item }) => (
-        <Swipeable
-    renderRightActions={() => (
-      <TouchableOpacity
-        style={styles.deleteButton}
-        onPress={() => deleteNote(item.id)}
-      >
-          <Icon name="trash-2" size={30} color="#fff"/>
-      </TouchableOpacity>
-    )}
-    >
-        <TouchableOpacity onPress={() => {
-            // openEditModal(item)
-        }}>
-            <Card style={{marginVertical: 5,borderRadius: 5, borderWidth: 0.3, borderColor: '#343434', backgroundColor: '#fff'}}>
-                <View style={{flexDirection: 'row', alignItems:'center', padding: 10}}>
-                    <View>
-                        <Text style={styles.title}>{item.title}</Text>
-                        <Text style={styles.item} numberOfLines={1} ellipsizeMode='tail'>{item.content}</Text>
-                    </View>
-                </View>
-            </Card>
-        </TouchableOpacity>
-          </Swipeable>
-    );
+    const openEditModal = (item) => {
+        setEditItem(item);
+        setTitle(item.title)
+        setContent(item.content)
+        setEditNoteModal(true)
+    }
 
     const displayNotes = () => {
         if (notes.length == 0)
@@ -87,6 +81,44 @@ const NotesScreen = () => {
                 <FlatList data={notes} renderItem={renderItems} keyExtractor={(note) => note.id.toString()} />
             )
     }
+    
+    const clearInput = () => {
+        setTitle('')
+        setContent('')
+    }
+    const renderItems = ({ item }) => (
+        <Swipeable
+            renderRightActions={() => (
+            <TouchableOpacity
+            style={styles.deleteButton}
+            onPress={() => deleteNote(item.id)}
+            >
+            <Icon name="trash-2" size={30} color="#fff"/>
+            </TouchableOpacity>
+            )}
+        >
+            <TouchableOpacity onPress={() => {
+                openEditModal(item)
+            }}>
+                <Card style={{marginVertical: 5,borderRadius: 5, borderWidth: 0.3, borderColor: '#343434', backgroundColor: '#fff'}}>
+                    <View style={{flexDirection: 'row', alignItems:'center', padding: 10}}>
+                        <View>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <Text style={styles.item} numberOfLines={1} ellipsizeMode='tail'>{item.content}</Text>
+                        </View>
+                    </View>
+                </Card>
+            </TouchableOpacity>
+        </Swipeable>
+    );
+    // reset everything when modals are closed
+    useEffect(() => {
+        if (!editNoteModal && !addNoteModal)
+        {
+            clearInput()
+        }
+      }, [addNoteModal, editNoteModal])
+
 
     return(
         <SafeAreaView style={styles.container}>
@@ -108,20 +140,57 @@ const NotesScreen = () => {
 
 
             <Modal animationType="slide" visible={addNoteModal}>
-                
                 <View style={styles.root}>
-                    <View style={{flexDirection:"row", justifyContent:'flex-end', alignItems: "center", marginRight: 25}}>
-                            <TouchableOpacity onPress={() => {
-                                addNote()
-                            }}>
-                            <Text style={{color: '#ff9900', fontSize: 20}}>Add</Text>
-                            </TouchableOpacity>
+                    <View style={{flexDirection:"row", justifyContent:'space-between', alignItems: "center", marginHorizontal: 15}}>
+                        <TouchableOpacity onPress={() => {
+                            setAddNoteModal(false)
+                        }}>
+                            {/* <Icon name="chevron-left" size={40} color="#ff9900"/> */}
+                        <Text style={{color: '#ff9900', fontSize: 20}}>Cancel</Text>
+                            
+                        </TouchableOpacity>
 
+                        <TouchableOpacity onPress={() => {
+                            addNote()
+                        }}>
+                            <Text style={{color: '#ff9900', fontSize: 20}}>Add</Text>
+                        </TouchableOpacity>
                     </View>
 
                     <View style={styles.lowerContent}>
 
-                        <View style={{width: '100%', paddingVertical: 10, paddingHorizontal: 20}}>
+                        <View style={{width: '100%', paddingVertical: 20, paddingHorizontal: 20}}>
+                            <TextInput placeholder="Title" value={title} onChangeText={title => setTitle(title)} autoCapitalize="none" fontSize="32" fontWeight='bold'/>
+                            <TextInput placeholder="Content" value={content} scrollEnabled={true} onChangeText={content => setContent(content)} autoCapitalize="none" multiline={true} fontSize="16" style={{marginBottom: 100}}/>
+                        </View>
+                        <View style={{marginTop: 30, justifyContent: "center", alignItems: 'center', backgroundColor: '#fff', width: 300}}>
+                            <Text style={{color: 'red'}}></Text>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal animationType="slide" visible={editNoteModal}>
+                <View style={styles.root}>
+                    <View style={{flexDirection:"row", justifyContent:'space-between', alignItems: "center", marginHorizontal: 15}}>
+                        <TouchableOpacity onPress={() => {
+                            setEditNoteModal(false)
+                        }}>
+                            {/* <Icon name="chevron-left" size={40} color="#ff9900"/> */}
+                        <Text style={{color: '#ff9900', fontSize: 20}}>Cancel</Text>
+                            
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => {
+                            editNote()
+                        }}>
+                            <Text style={{color: '#ff9900', fontSize: 20}}>Done</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.lowerContent}>
+
+                        <View style={{width: '100%', paddingVertical: 20, paddingHorizontal: 20}}>
                             <TextInput placeholder="Title" value={title} onChangeText={title => setTitle(title)} autoCapitalize="none" fontSize="32" fontWeight='bold'/>
                             <TextInput placeholder="Content" value={content} scrollEnabled={true} onChangeText={content => setContent(content)} autoCapitalize="none" multiline={true} fontSize="16" style={{marginBottom: 100}}/>
                         </View>
@@ -160,7 +229,6 @@ const styles = StyleSheet.create({
     },
     root: {
         flexDirection: "column",
-
         height: '100%',
         marginTop: 50
 
