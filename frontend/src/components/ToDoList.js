da2b0f88cfc34b6da6e107d7ee8e141f1fe902bd
Nicolas Rodriguez <nicorb02@gmail.com>
2023-04-28 23:08:27 -0400

    completed web todo (all functionality)

--- frontend/src/components/ToDoList.js
+++ frontend/src/components/ToDoList.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import '../styles/ToDo.css'
+import AddTodo from './AddToDo';
 import CloseEvent from '../styles/assets/CloseEvent';
 import Trash from '../styles/assets/Trash'
 
@@ -14,56 +15,158 @@ const ToDoList = ({ setDisplayToDo }) => {
     const _id = userData.id;
     const jwtToken = storage.retrieveToken();
 
-    async function handleAddToDO () {
-        const response = await fetch(bp.buildPath('/api/addToDo'), {
+    const [list, setList] = useState([]);
+    const [reloadTasks, setReloadTasks] = useState(false);
+
+    // read todos api and populate list (useEffect on reloadTasks)
+    useEffect (() => {
+        async function handleReadTasks () {
+            const response = await fetch(bp.buildPath('/api/readToDo'), {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ _id, jwtToken })
+            });
+
+            const data = await response.json();
+
+            const listTemp = data.results
+                .map((todo) => ({
+                    name: todo.title,
+                    complete: todo.complete
+                }));
+            setList(listTemp);
+        }
+
+        handleReadTasks();
+    }, [reloadTasks]);
+    
+    // make delete functionality and connect to api  (toggle reload when done)
+    async function handleDeleteTask () {
+        const response = await fetch(bp.buildPath('/api/delToDo'), {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({ _id, jwtToken })
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ _id, title, jwtToken })
         });
+
         const data = await response.json();
+
+        if (data.error === '')
+        {
+            console.log("good delete task")
+        }
+        else
+        {
+            console.log(data.error)
+        }
+        setTitle('');
+        toggleReloadTasks();
     }
 
-    // make component for adding a task (input name)
-    // connect to add todo api (make a variable to toggle for useEffect dependency on readToDos)
-    
-    // make delete functionality and connect to api  (make a variable to toggle for useEffect dependency on readToDos)
+    // make complete functionality and connect to api (toggle reload when done)
+    async function handleCompleteTask (task) {
+        const response = await fetch(bp.buildPath('/api/completeToDo'), {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ _id, title: task.name , jwtToken })
+        });
+
+        const data = await response.json();
+
+        if (data.error === '')
+        {
+            console.log("good complete task")
+        }
+        else
+        {
+            console.log(data.error)
+        }
+        setTitle('');
+        toggleReloadTasks();
+    }
+
+    // make incomplete functionality and connect to api (toggle reload when done)
+    async function handleIncompleteTask (task) {
+        const response = await fetch(bp.buildPath('/api/incompleteToDo'), {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ _id, title: task.name , jwtToken })
+        });
+
+        const data = await response.json();
+
+        if (data.error === '')
+        {
+            console.log("good incomplete task")
+        }
+        else
+        {
+            console.log(data.error)
+        }
+        setTitle('');
+        toggleReloadTasks();
+    }
+
+    // checks a task to call complete or incomplete api
+    function checkCompletion (task) {
+        // if task is incomplete, complete it
+        if (task.complete == 0)
+        {
+            handleCompleteTask(task);
+        }
+        // task is complete, incomplete it
+        else
+        {
+            handleIncompleteTask(task);
+        }
+    }
     
     // make useStates for getting title to delete and etc..
+    const [title, setTitle] = useState('');
+    const [displayAddToDo, setDisplayAddTodo] = useState(false);
 
-    let list = [
-        {title: 'Numero one', complete: '0' },
-        {title: 'Two', complete: '0' },
-        {title: 'Tres/three', complete: '1' },
-        {title: 'Quatro', complete: '1' },
-        {title: 'Cinco five', complete: '0' },
-        {title: '6 six', complete: '0' },
-        {title: 'Numero one', complete: '0' },
-        {title: 'Two', complete: '0' },
-        {title: 'Tres/three', complete: '1' },
-        {title: 'Quatro', complete: '1' },
-        {title: 'Cinco five', complete: '0' },
-        {title: '6 six', complete: '0' }
-    ]
-    
+    // function to reload tasks (calls readTodo api)
+    function toggleReloadTasks () {
+        if (reloadTasks) setReloadTasks(false);
+        else setReloadTasks(true);
+    }
+
+    // for setting title for trash can and delete api parameter
+    const handleTaskClick = useCallback((task) => {
+        return () => setTitle(task.name);
+    }, []);
+
+    // hide todo list
     function toggleDisplayToDo () {
         setDisplayToDo(false);
     }
 
+    // display add todo component
+    function toggleDisplayAddTodo () {
+        setDisplayAddTodo(true);
+    }
+
     return (
             <div className='tasks-container'>
+                <div className={`add-todo-component ${displayAddToDo ? '' : 'hide-todo-component'}`}>
+                    <AddTodo 
+                        setReloadTasks={setReloadTasks}
+                        reloadTasks={reloadTasks}
+                        setDisplayAddTodo={setDisplayAddTodo}
+                    />
+                </div>
                 <div className='close-task' onClick={toggleDisplayToDo}>
                     <CloseEvent />
                 </div>
                 <div className='tasks-list'>
                     {list.map((task) => (
                         <li className='task-item'>
-                            <div className={task.complete == 1 ? 'task-complete' : 'task-incomplete'} ></div>
-                            <p className={task.complete == 1 ? 'task-title-complete' : 'task-title'}>{task.title}</p>
-                            <div className='delete-button'> <Trash /> </div>
+                            <div className={task.complete == 1 ? 'task-complete' : 'task-incomplete'} onClick={() => checkCompletion(task)}></div>
+                            <p className={task.complete == 1 ? 'task-title-complete' : 'task-title'} onClick={handleTaskClick(task)}>{task.name}</p>
+                            <div className={title === task.name ? 'delete-button' : 'delete-button-hidden'} onClick={handleDeleteTask}> <Trash /> </div>
                         </li>
                     ))}
                 </div>
-                <button type='button' className='add-task-button'>Add Task</button>
+                <button type='button' className='add-task-button' onClick={toggleDisplayAddTodo}>Add Task</button>
             </div>
     );
 };
