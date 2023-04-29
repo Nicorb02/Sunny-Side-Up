d81ed02d499073c8fb3dd38e78bf7f04fcd653be
Nicolas Rodriguez <nicorb02@gmail.com>
2023-04-29 19:36:39 -0400

    another add svg, working on notes

--- frontend/src/pages/TestingPage.js
+++ frontend/src/pages/TestingPage.js
@@ -1,38 +1,30 @@
 import React, { useState } from 'react';
-import '../styles/AddToDo.css'
-import CloseEvent from '../styles/assets/CloseEvent';
+import '../styles/Notes.css'
+import Trash from '../styles/assets/Trash'
+import AddIcon from '../styles/assets/AddIcon';
 
 const TestingPage = () => {
-    // // import buildPath and local storage functions
-    // let bp = require('./Path.js');
-    // var storage = require('../tokenStorage.js');
 
-    // // retrieve user data and current jswt from local storage
-    // const userData = JSON.parse(localStorage.getItem('user_data'));
-    // const _id = userData.id;
-    // const jwtToken = storage.retrieveToken();
-
-    // async function handleAddToDO () {
-    //     const response = await fetch(bp.buildPath('/api/addToDo'), {
-    //         method: 'POST',
-    //         headers: { 'Content-Type': 'application/json'},
-    //         body: JSON.stringify({ _id, jwtToken })
-    //     });
-    //     const data = await response.json();
-    // }
-
-
-
-    const [title, setTitle] = useState('');
+    let notes = [
+        {name: 'one', content: 'random string lols lasodslfsdf'},
+        {name: 'dos', content: 'im awesomeodmasfsdfsdf'},
+        {name: 'tresssss', content: 'whats seems to to be the occifer problem?'},
+        {name: 'fourfourfour', content: 'random string lols lasodslfsdf'},
+    ];
 
     return (
-            <div className='add-task-container'>
-                <div className='close-add-task'> <CloseEvent /> </div>
-                <div className='task-name-container'>
-                    <input type='text' className='task-name-input' placeholder='Task Name' value={title} onChange={e => setTitle(e.target.value)}></input>
+            <div className='notes-container'>
+                <div className='notes-list'>
+                    {notes.map((note) => (
+                        <li className='listed-note'>
+                            <div className='listed-note-name'>{note.name}</div>
+                            <div className='delete-note'> <Trash /> </div>
+                        </li>
+                    ))}
+                    <AddIcon />
                 </div>
-                <div className='add-task-button-container'> 
-                    <button type='button' className='add-task-button'>Add</button>
+                <div className='current-note-container'>
+                    <p className='current-note-string'></p>
                 </div>
             </div>
     );
---
+++ frontend/src/styles/assets/NewNoteIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewNoteIcon = () => {
+    return (
+        <svg width={36} height={36} fill="#f7fff7" viewBox="0 0 24 24">
+            <path fillRule="evenodd" d="M12 21.6a9.6 9.6 0 1 0 0-19.2 9.6 9.6 0 0 0 0 19.2Zm1.2-13.2a1.2 1.2 0 1 0-2.4 0v2.4H8.4a1.2 1.2 0 0 0 0 2.4h2.4v2.4a1.2 1.2 0 1 0 2.4 0v-2.4h2.4a1.2 1.2 0 1 0 0-2.4h-2.4V8.4Z" clipRule="evenodd" />
+        </svg>
+    )
+}
+
+export default NewNoteIcon;
