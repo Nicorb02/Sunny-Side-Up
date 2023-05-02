a2a694234abc544505a2ac724ad4177fd22fd35a
jenna3708 <jennabusch3708@gmail.com>
2023-05-02 00:05:03 -0400

    new jenna branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import PlannerPage from './pages/PlannerPage';
+import TestingPage from './pages/TestingPage';
+import Contacts from './components/Contacts';
+import AgendaPage from './pages/AgendaPage';
 import About from './components/About';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, {useState} from "react";
+
 
 
 function App() {
@@ -11,6 +17,8 @@ function App() {
       <Routes>
         <Route path="/" index element={<LoginPage />} />
         <Route path="/PlannerPage" element={<PlannerPage />} />
+        <Route path="/Contacts" element={<Contacts />} />
+        <Route path="/AgendaPage" element={<AgendaPage />} />
         <Route path="/About" element={<About />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/styles/assets/MonthView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MonthView = () => {
+    return (
+        <svg width={36} height={36} fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2Z" />
+        </svg>
+    )
+}
+
+export default MonthView;
