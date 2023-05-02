e1c220f691db23f29ccf4bde4ef2eee4fc891e5d
jenna3708 <jennabusch3708@gmail.com>
2023-05-02 13:49:16 -0400

    Auto stash before merge of "jenna" and "origin/jenna"

---
+++ frontend/src/styles/assets/ExitEdit.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ExitEdit = () => {
+    return (
+        <svg width={36} height={36} fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="m10 14 2-2m0 0 2-2m-2 2-2-2m2 2 2 2m7-2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+    )
+}
+
+export default ExitEdit;
+
+<svg width={36} height={36} fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <path d="m10 14 2-2m0 0 2-2m-2 2-2-2m2 2 2 2m7-2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+
